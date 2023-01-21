b99d2577247fa03cb1b55a2e53f12e518901bceb
xmahely <xmahelyova@stuba.sk>
2023-01-21 17:15:47 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,75 @@
 //
 // Scripts
 //
+// let fontSize = $("#fontSize");
+//
+// fontSize.focusout(function() {
+//     let newSize = fontSize.val();
+//     fontSize.css("fontSize", newSize + "rem");
+// });
 
-
-let editor;
+function magnify(imgID, zoom) {
+    var img, glass, w, h, bw;
+    // img = document.getElementById(imgID);
+    img = $(imgID);
+    console.log(img.naturalWidth);
+    /*create magnifier glass:*/
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "img-magnifier-glass");
+    /*insert magnifier glass:*/
+    img.parentElement.insertBefore(glass, img);
+    /*set background properties for the magnifier glass:*/
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = (img.naturalWidth * zoom) + "px " + (img.naturalHeight * zoom) + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+    /*execute a function when someone moves the magnifier glass over the image:*/
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+    /*and also for touch screens:*/
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
+    function moveMagnifier(e) {
+        var pos, x, y;
+        /*prevent any other actions that may occur when moving over the image*/
+        e.preventDefault();
+        /*get the cursor's x and y positions:*/
+        pos = getCursorPos(e);
+        x = pos.x;
+        y = pos.y;
+        /*prevent the magnifier glass from being positioned outside the image:*/
+        if (x > img.naturalWidth - (w / zoom)) {x = img.naturalWidth - (w / zoom);}
+        if (x < w / zoom) {x = w / zoom;}
+        if (y > img.naturalHeight - (h / zoom)) {y = img.naturalHeight - (h / zoom);}
+        if (y < h / zoom) {y = h / zoom;}
+        /*set the position of the magnifier glass:*/
+        glass.style.left = (x - w) + "px";
+        glass.style.top = (y - h) + "px";
+        /*display what the magnifier glass "sees":*/
+        glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    }
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /*get the x and y positions of the image:*/
+        a = img.getBoundingClientRect();
+        /*calculate the cursor's x and y coordinates, relative to the image:*/
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /*consider any page scrolling:*/
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return {x : x, y : y};
+    }
+}
 
 window.addEventListener('DOMContentLoaded', event => {
+
+    //popper
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
     // Navbar shrink function
     let navbarShrink = function () {
@@ -18,7 +82,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
         let logos = document.getElementsByClassName('hc-logo');
         if (!navbarCollapsible) {
-            console.log("neniii");
             return;
         }
         if (window.scrollY === 0) {
